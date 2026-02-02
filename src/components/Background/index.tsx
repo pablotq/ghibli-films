c20e002751200d1bd63dad68f5291fb70b2889cd
pablotq <pablo.cstds@gmail.com>
2026-02-02 09:44:03 -0300

    css trnasformado em module.css

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,27 +1,32 @@
-import React from "react";
-import "./leaves.css";
+import React, { useMemo } from "react";
+import styles from "./leaves.module.css";
 
 export const Background: React.FC<React.PropsWithChildren<Record<string, unknown>>> = ({ children }) => {
+    // Gera delays aleatórios apenas uma vez no mount do componente
+    const leaves = useMemo(() => 
+        Array.from({ length: 30 }, (_, index) => ({
+            id: index,
+            // eslint-disable-next-line react-hooks/purity
+            delay: Math.random() * 25
+        })),
+        [] // Dependency array vazio = executa apenas uma vez
+    );
+
     return (
-        <div className="bg-wrapper">
-            <div id="leaves">
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
-                <i></i>
+        <div className={styles.bgWrapper}>
+            <div className={styles.leavesContainer}>
+                {leaves.map((leaf) => (
+                    <i 
+                        key={leaf.id}
+                        className={styles.leaf}
+                        style={{
+                            animationDelay: `${leaf.delay}s`,
+                            WebkitAnimationDelay: `${leaf.delay}s`
+                        }}
+                    ></i>
+                ))}
             </div>
-            <div className="bg-content">
+            <div className={styles.bgContent}>
                 {children}
             </div>
         </div>
