6398c5426c86faaf128f238bb26e0a12b008a4b8
pablotq <pablo.cstds@gmail.com>
2026-02-02 14:38:32 -0300

    remoção do background

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "../Header"
-import { Background } from "../Background"
 
 export const Layout = () => {
     return (
-        <Background>
+        <>
             <Header />
             <Outlet />
-        </Background>
+        </>
     )
 }
